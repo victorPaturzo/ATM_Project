78bc9a1819d317483d8e4e16d881813887f66bf3
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-01 16:14:18 -0400

    Completed mainMenu baseplate.  Now in testing phase. (VPP

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const prompt = require("prompt-sync")();
 //TODO: Utilize prompt-sync so we can get user input for various functions
 //* Refer to Intro to Node.js PowerPoint for prompt-sync installation instructions
 
-function accessATM(Boolean) {
+function accessATM() {
   let pin = prompt("Please enter your pin.");
   verify(pin);
-  if (pin === true) {
-    return pin
+  if (parseInt(pin) === true) {
+    return true
   } else {alert("Invalid pin.  Please try again");
     accessATM()}
   //TODO: Prompt users for their pin
@@ -29,7 +29,30 @@ function mainMenu() {
   "1: Check current balance" + "\n" + "2: Make a deposit" + "\n" + "3: Make a withdrawal" 
   + "\n" + "4: Restart application" + "5: Quit");
   switch (mainMenuSelection) {
-    case "1": check()
+    case "1": 
+      check(accessATM);
+      mainMenu();
+      break;
+    case "2":
+      let depositAmount = prompt("How much would you like to deposit into your account?");
+      add(depositAmount, accessATM);
+      mainMenu;
+      break;
+    case "3":
+      let withdrawalAmount = prompt("How much would you like to withdraw from your account?");
+      subtract(withdrawalAmount, accessATM);
+      mainMenu()
+      break;
+    case "4":
+      mainMenu();
+      break;
+    case "5":
+      alert("Thank you for your continued patronage!  See you next time!");
+      break;
+    default:
+      alert("Invalid entry, please try again.");
+      mainMenu();
+      break;
   }
   //TODO: Set up a main menu.  Prompt users for ATM selection to do the following:
   //! Remember - we should keep prompting the user for options until they quit!
@@ -39,5 +62,5 @@ function mainMenu() {
   //Restart
   //Quit
 }
-
+mainMenu();
 //TODO: Call mainMenu function to start our app!
